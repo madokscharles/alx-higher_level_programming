9fd90a3a34e02bcdef48261a61d3461cea835b7d
madokscharles <charlesmadokes@gmail.com>
2022-12-05 15:01:57 -0800

    A function in C that checks if a singly linked list is a palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,4 +1,28 @@
 #include "lists.h"
+#include <stdlib.h>
+
+/**
+ * reverse_array - reverses the content of an array of integers
+ * @a: int array to reverse
+ * @n: number of elements in the array
+ *
+ * Return: concatenated string
+ */
+void reverse_array(int *a, int n)
+{
+	int *begin = a;
+	int *end;
+	int hold = 0;
+
+	end = a + n - 1;
+	for (; begin < end; begin++, end--)
+	{
+		hold = *end;
+		*end = *begin;
+		*begin = hold;
+	}
+}
+
 
 /**
  * is_palindrome - finds if linked list is palindrome
@@ -11,42 +35,26 @@
 
 int is_palindrome(listint_t **head)
 {
-	listint_t *mover, *front, *back, *half;
-	int count = 0, move = 0;
+	int size, *list, *rev;
+	listint_t *copy = *head;
 
-	if (*head == NULL || (*head)->next == NULL)
+	if (!head || !copy)
+		return (0);
+	if (!copy->next)
 		return (1);
 
-	front = *head;
-	back = *head;
-	half = *head;
+	list = malloc(sizeof(int *));
+	if (!list)
+		return (0);
+	rev = malloc(sizeof(int *));
+	if (!rev)
+		return (0);
+	for (size = 0; copy; copy = copy->next, size++)
+		list[size] = copy->n;
 
-	while (back->next != NULL)
-	{
-		count++;
-		back = back->next;
-	}
-	while (move < count)
-	{
-		half = half->next;
-		move++;
-		count--;
-	}
-
-	mover = half;
-
-	while (front != back)
-	{
-		if (front->n != back->n)
-			return (0);
-		if (front->next == back)
-			return (1);
-		while (mover->next != back)
-			mover = mover->next;
-		back = mover;
-		front = front->next;
-		mover = half;
-	}
-
-	return (1);
+	list = rev;
+	reverse_array(rev, size);
+	if (list == rev)
+		return (1);
+	return (0);
 }
