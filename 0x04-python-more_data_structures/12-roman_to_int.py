116b453c002fc64c60826d34ffe93c425f085370
madokscharles <charlesmadokes@gmail.com>
2022-12-08 08:38:55 -0800

    Technical interview preparation

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,27 +1,22 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string is None:
+    if (not isinstance(roman_string, str) or
+            roman_string is None):
         return 0
-    if (isinstance(roman_string, str)) is False:
-        return 0
-    roman_nums = {"I": 1,
-            "V": 5,
+    roman_numerals = {"I": 1,
+            "V ": 5,
             "X": 10,
             "L": 50,
             "C": 100,
             "D": 500,
             "M": 1000}
-    nextR = ""
     num = 0
     for i in range(len(roman_string)):
-        if roman_string[i] in roman_nums.keys():
-            key = roman_string[i]
-            if i < len(roman_string) - 1:
-                nextR = roman_string[i + 1]
-            if i == len(roman_string) - 1:
-                num += roman_nums[key]
-            elif roman_nums[key] < roman_nums[nextR]:
-                num -= roman_nums[key]
-            else:
-                num += roman_nums[key]
+        if roman_numerals.get(roman_string[i], 0) == 0:
+            return 0
+        if (i != (len(roman_string) - 1) and
+                roman_numerals[roman_string[i]] > roman_numerals[roman_string[i + 1]]):
+            num += roman_numerals[roman_string[i]] * -1
+        else:
+            num += roman_numerals[roman_string[i]]
     return num
