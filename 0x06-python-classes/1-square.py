b6c82b9da546b84107b57818303c03963f506c25
madokscharles <charlesmadokes@gmail.com>
2022-12-20 12:04:34 -0800

    A class Square that defines a square by: (based on 0-square.py)

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# 1-square.py by Madoks Charles
+"""Defines a Square based on 0-square.py"""
+
+
+class Square:
+    """Represents a Square"""
+
+    def __init__(self, size):
+        """Initializing Square class
+        Size - Private instance attribute
+        """
+
+        self.__size = size
