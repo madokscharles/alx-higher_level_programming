2733c2863c136275152e66cab85725ebf82a80fc
madokscharles <charlesmadokes@gmail.com>
2023-01-03 11:59:51 -0800

    A class LockedClass with no class or object attribute

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
-"""Defines a locked class"""
-
-
 class LockedClass:
-    """
-    Prevents user from creating new instance, except first_name
-    """
-
-     __slots__ = ["first_name"]
+    __slots__ = "first_name"
