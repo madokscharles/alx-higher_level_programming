2ec0d9f2c171420c5a5f259d50e8223d6fbfd234
madokscharles <charlesmadokes@gmail.com>
2023-01-09 14:28:28 -0800

    function that returns the list of available attributes and methods of an object

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+    This function returns the lists of available attributes
+    and methods of an object.
+"""
+
+
+def lockup(obj):
+    """ Function looks out for attributes and methods of an obj """
+    return dir(obj)
