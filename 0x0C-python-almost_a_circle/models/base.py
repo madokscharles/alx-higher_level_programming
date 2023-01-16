e48372933db9d32877862636dfd1569764637d44
madokscharles <charlesmadoks@gmail.com>
2023-01-16 13:51:01 -0800

    The first class Base

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" This module contains a class to serve as base for other classes """
+
+
+import csv
+import json
+import os
+import turtle
+
+
+class Base:
+    """
+    Represents base of classes created
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
