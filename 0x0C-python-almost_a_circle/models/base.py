833cf3418785741b83d6375649d8812a15f0ea4e
madokscharles <charlesmadoks@gmail.com>
2023-01-16 17:41:08 -0800

    Fix bug

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -8,14 +8,16 @@ import turtle
 
 class Base:
     """ Represents base of all classes created """
+
     __nb_objects = 0
 
     def __init__(self, id=None):
-        if id is None:
-            type(self).__nb_objects += 1
-            self.id = self.__nb_objects
-        else:
+        """ """
+        if id is not None:
             self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
 
     @staticmethod
     def to_json_string(list_dictionaries):
