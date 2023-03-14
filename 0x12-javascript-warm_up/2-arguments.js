f7329c044feee21795b02287ddbf566bbf476e70
madokscharles <charlesmadoks@gmail.com>
2023-03-14 02:49:33 +0100

    process.argv

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,13 +1,10 @@
 #!/usr/bin/node
 // Script prints a message depending on number of arguments passed
 
-// Importing the process module
-const process = require('process');
-
 // Printing value for process.argv
-const myArgs = process.argv;
+const myArgs = process.argv.length;
 
-if (myArgs.length > 2) {
+if (myArgs > 2) {
   console.log('Argument' + (myArgs > 3 ? 's' : '') + ' found');
 } else {
   console.log('No argument');
