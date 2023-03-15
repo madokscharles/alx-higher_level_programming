1b8b3186446ee45dd3652ea4d4fbbd2e98141e3d
madokscharles <charlesmadoks@gmail.com>
2023-03-16 00:58:06 +0100

    Esrever

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// function that returns the reversed version of a list
+
+exports.esrever = function (list) {
+  const rev = [];
+
+  for (let i = list.length - 1; i >= 0; i--) {
+    rev.push(list[i]);
+  }
+
+  return rev;
+};
