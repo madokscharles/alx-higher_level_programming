1e26761075bca446867a90318f25f020f7c2c958
madokscharles <charlesmadoks@gmail.com>
2023-03-16 00:08:22 +0100

    Rectangle #3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -8,4 +8,10 @@ module.exports = class Rectangle {
       this.height = h;
     }
   }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
 };
