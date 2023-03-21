10424d044335b6564a3e3d8d7ff63bfcad3087f5
madokscharles <charlesmadoks@gmail.com>
2023-03-21 11:23:24 -0700

    Script lists states from database . checker correction

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-'''
-Script lists all states from the database `hbtn_0e_0_usa'
-'''
+"""
+Script lists all states from the database
+`hbtn_0e_0_usa'
+"""
 
 import MySQLdb
 from sys import argv
 
 if __name__ == '__main__':
-    '''
+    """
     Accesses the database
-    '''
+    """
     db_connect = MySQLdb.connect(
             host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3])
 
