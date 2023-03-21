b9307f5ddac620244ddfd2bc1981fe805d23aa04
madokscharles <charlesmadoks@gmail.com>
2023-03-21 12:44:59 -0700

    Fix errors for script listing states from database

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,24 +1,29 @@
 #!/usr/bin/python3
 """
-Script lists all states from the database
-`hbtn_0e_0_usa'
+Script lists all states from the database `hbtn_0e_0_usa'
 """
 
-import MySQLdb
-from sys import argv
-
 if __name__ == '__main__':
+    import MySQLdb
+    from sys import argv as av
+
     """
-    Accesses the database
+    Accesses the database and connects to database
     """
-    db_connect = MySQLdb.connect(
-            host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3])
+    db_connect = MySQLdb.connect(host="localhost",
+            user=av[1],
+            port=3306,
+            passwd=av[2],
+            db=av[3])
 
-    db_cursor = db_connect.cursor()
+    db_cur = db_connect.cursor()
 
-    db_cursor.execute("SELECT * FROM states")
+    db_cur.execute("SELECT * FROM states ORDER BY id")
 
-    rows_selected = db_cursor.fetchall()
+    ''' fetch all at once '''
+    rows_selected = db_cur.fetchall()
 
     for row in rows_selected:
-        print(row)
+        print(f"{row}")
+    db_cur.close()
+    db_connect.close()
