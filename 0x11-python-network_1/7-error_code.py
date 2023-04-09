112865a1b6c290fd4cad81f3f72da16cf49a80ea
madokscharles <charlesmadoks@gmail.com>
2023-04-09 08:45:09 -0700

    Script takes in a URL, sends a request to it

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Script takes in a URL, sends a request to it and displays response """
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    response = requests.get(argv[1])
+
+    if response.status_code >= 400:
+        print("Error code: {}".format(response.status_code))
+    else:
+        print(response.text)
