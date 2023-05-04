15cf95fe8ad235fff1e4859cebe3eb37badb2979
Madoks Charles <111252523+madokscharles@users.noreply.github.com>
2023-05-04 22:21:25 +0100

    Delete 0x11-python-network_1 directory

--- 0x11-python-network_1/5-hbtn_header.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-""" Script takes in a URL, sends a request to it and displays value """
-import requests
-from sys import argv
-
-if __name__ == "__main__":
-    r = requests.get(argv[1]).headers.get("X-Request-Id")
-    print(r)
--- 0x11-python-network_1/6-post_email.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-""" Script takes in a URL and an email and sends a POST request """
-import requests
-from sys import argv
-
-if __name__ == "__main__":
-    response = requests.post(argv[1], data={'email': argv[2]})
-    print(response.text)
