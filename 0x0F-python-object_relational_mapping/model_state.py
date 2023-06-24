aef4ec2e303ab0608eb9766acc16ba9dea16963e
madokscharles <charlesmadoks@gmail.com>
2023-06-24 05:45:59 -0700

    Checker error - failed requirement

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -4,12 +4,12 @@ This script defines a State class and a
 Base class
 """
 
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 
 
-
-Base = declarative_base()
+mymetadata = MetaData()
+Base = declarative_base(metadata=mymetadata)
 
 
 class State(Base):
@@ -24,6 +24,7 @@ class State(Base):
     __tablename__ = 'states'
 
     id = Column(Integer,
+                unique=True,
                 primary_key=True,
                 nullable=False)
     name = Column(String(128), nullable=False)
