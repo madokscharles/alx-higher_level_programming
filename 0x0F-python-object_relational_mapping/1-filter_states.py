755c72b7a35e107971930d9bcfb103cd57102a9d
madokscharles <charlesmadoks@gmail.com>
2023-06-24 03:25:34 -0700

    checker error

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,32 +1,33 @@
 #!/usr/bin/python3
 """
 This script that lists all states with a name
-starting with N (upper N) from the database hbtn_0e_0_usa
+starting with N (upper N) from the database
 """
 
-import MySQLdb
-import sys
+import MySQLdb as db
+from sys import argv
 
-""" Connects to database, gets states """
 
 if __name__ == '__main__':
+    """
+    Connects to database, gets states
+    """
     if __name__ == "__main__":
-        db_connect = MySQLdb.connect(
+        db_connect = db.connect(
                 host="localhost",
                 port=3306,
-                user=sys.argv[1],
-                passwd=sys.argv[2],
-                db=sys.argv[3]
+                user=argv[1],
+                passwd=argv[2],
+                db=argv[3]
                 )
 
         db_cur = db_connect.cursor()
 
-        db_cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' \
-                ORDER BY states.id ASC")
+        db_cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id")
 
         selected = db_cur.fetchall()
-        for row in selected :
-            print(row)
+        for row in selected:
+            print("{}".format(row))
 
         db_cur.close()
         db_connect.close()
