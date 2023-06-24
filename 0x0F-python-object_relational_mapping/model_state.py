3721eb370ed9444a34399e20f115afbfbe07c9ca
madokscharles <charlesmadoks@gmail.com>
2023-06-24 05:27:30 -0700

    Checker error - failed requirement

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -24,6 +24,6 @@ class State(Base):
     __tablename__ = 'states'
 
     id = Column(Integer,
-                primary_key=True
+                primary_key=True,
                 nullable=False)
     name = Column(String(128), nullable=False)
