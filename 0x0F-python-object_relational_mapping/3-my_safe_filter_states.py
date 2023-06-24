0a19d80f1952735ee299194b28e24737f086da71
madokscharles <charlesmadoks@gmail.com>
2023-06-24 03:49:07 -0700

    filters states by user input

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+This script takes in an argument and displays
+all values in the state table of the database
+"""
+
+import MySQLdb as db
+from sys import argv
+
+
+if __name__ == '__main__':
+    """
+    Connects to database, gets states
+    """
+    if __name__ == "__main__":
+        db_connect = db.connect(
+                host="localhost",
+                port=3306,
+                user=argv[1],
+                passwd=argv[2],
+                db=argv[3]
+                )
+
+        db_cur = db_connect.cursor()
+
+        db_cur.execute("SELECT * FROM states WHERE name = %s \
+                    ORDER BY states.id", (argv[4],))
+
+        selected = db_cur.fetchall()
+        for row in selected:
+            print("{}".format(row))
+
+        db_cur.close()
+        db_connect.close()
