7d60ec1212857d07e9ee95d2487986875f02b934
madokscharles <charlesmadoks@gmail.com>
2023-07-06 17:03:28 -0700

    Using GitHub API to list 10 commits of the repo rails by user rails

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Script takes 2 arguments
+Lists 10 commits (from the most recent to oldest) of the repository “rails” by the user “rails”
+"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(argv[2], argv[1])
+
+    response = requests.get(url)
+    commits = response.json()
+
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
