0bc17c310c0a6e46c966f22ec9aca581efc59f0a
madokscharles <charlesmadoks@gmail.com>
2023-08-02 12:45:01 -0700

    jQuery API

--- 0x15-javascript-web_jquery/2-script.js
+++ 0x15-javascript-web_jquery/2-script.js
@@ -1,10 +1,5 @@
-// Start by waiting for the document to be ready
-$(document).ready(function() {
-    const redHeader = $("#red_header");
-    // Adding click event handler
-    redHeader.click(function() {
-        const headerColor = $("header");
-        // Changing color to red
-        headerColor.css("color", "#FF0000");
-    });
+$document.ready(function () {
+  $('DIV#red_header').click(function () {
+    $('header'.css('color', '#FF0000'));
+  });
 });
