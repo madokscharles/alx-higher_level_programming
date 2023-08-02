3624bd2cc0f74213ece723ef6d7c14a61473cd0d
madokscharles <charlesmadoks@gmail.com>
2023-08-02 11:47:23 -0700

    Using jQuery API

---
+++ 0x15-javascript-web_jquery/1-script.js
@@ -0,0 +1,6 @@
+// Start by waiting for the document to be ready
+$(document).ready(function() {
+    const headerColor = $("header");
+    // Change text color
+    headerColor.css("color", "#FF0000");
+});
